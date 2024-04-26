bde64c03943ea1475aea0f40ee2b4b5f91e57b98
Joseph Gichohi <josephgichohi322@gmail.com>
2024-04-26 13:28:14 +0300

    updates on the api

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    
-    path('', include('api.urls'))
+    path('', views.home, name='home')
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,24 @@
+from django import http
 from django.shortcuts import render
 
+import requests
+API_KEY = '2f7465f8536c40cfb8f14889230503b4'
+
 # Create your views here.
+
+
+def home(request):
+    url = f'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+    articles = data['articles']
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'home.html', context)
+
+
