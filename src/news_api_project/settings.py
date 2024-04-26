72063f544cc7a52ad9307c6d8a7ab988cf37aa5c
Joseph Gichohi <josephgichohi322@gmail.com>
2024-04-26 11:29:43 +0300

    set app of the api app

--- src/news_api_project/settings.py
+++ src/news_api_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES = [
+    BASE_DIR / 'api/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
