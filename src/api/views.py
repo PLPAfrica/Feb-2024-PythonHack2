70dff071ddf07f949ea3f2af99e4aa9d7ce20e57
Joseph Gichohi <josephgichohi322@gmail.com>
2024-04-26 16:33:24 +0300

    sdfuiwgrf

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,4 @@
+
 from django import http
 from django.shortcuts import render
 
@@ -8,13 +9,23 @@ API_KEY = '2f7465f8536c40cfb8f14889230503b4'
 
 
 def home(request):
-    url = f'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    print(data)
+    country = request.GET.get('country')
+    category = request.GET.get('country')
+
+
+    if country:
+        url = f'http://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        print(data)
+        
+    else:
+        url = f'http://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        print(data)
 
     articles = data['articles']
-
     context = {
         'articles' : articles
     }
