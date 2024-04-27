c42908923f3a8f6496a02eaa8d8f4a4617b841c6
Travcort <tarvkoome@gmail.com>
2024-04-27 12:11:04 +0300

    Tarvone's(Travcort) Hackathon Submission

--- django_project/views.py
+++ django_project/views.py
@@ -1,17 +1,52 @@
 import requests
+import random
+from django.http import HttpRequest
 from django.shortcuts import render
+from decouple import config
 
-def home(request):
-  # USING APIS => Example 1
-  response = requests.get('https://api.github.com/events')
+def main(request: HttpRequest):
+               #THE WEATHER API
+  BASE_URL = 'https://api.weatherapi.com/v1/current.json?'
+  API_KEY = 'cf8227e228814bd894a131010242604'
+  CITY = str(request.GET.get('city', 'London'))
+  cityContext = CITY
+
+  url = BASE_URL + 'key=' + API_KEY + '&q=' + CITY
+  response = requests.get(url)
   data = response.json()
-  result = data[0]["repo"]
+  time = data["location"]["localtime"]
+  region = data["location"]["region"]
+  country = data["location"]["country"]
+  latitude = data["location"]["lat"]
+  longitude = data["location"]["lon"]
+  timezone = data["location"]["tz_id"]
+  current_time = data["current"]["last_updated"]
+  temp_degrees = data["current"]["temp_c"]
+  temp_farenheit = data["current"]["temp_f"]
+  weather_description = data["current"]["condition"]["text"]
+  wind_speedKPH = data["current"]["wind_kph"]
+  wind_speedMPH = data["current"]["wind_mph"]
+  humidity = data["current"]["humidity"]
 
-  # Example 2
-  reponse2 = requests.get('https://dog.ceo/api/breeds/image/random')
-  data2 = reponse2.json()
-  result2 = data2["message"]
+  weatherAPI =  {'time': time, 'region': region, 'country': country, 'latitude': latitude, 'longitude': longitude, 'timezone': timezone, 'current_time': current_time, 'temp_degrees': temp_degrees, 'temp_farenheit': temp_farenheit,'weather_description': weather_description, 'wind_speedKPH': wind_speedKPH, 'wind_speedMPH': wind_speedMPH, 'humidity': humidity}
 
 
-  
-  return render(request, 'templates/index.html', {'result': result, 'result2': result2})
+
+                  #THE RANDOM QUOTES API
+  resp = requests.get('https://type.fit/api/quotes')
+  QUOTES = resp.json()
+  quote_index = random.randint(0, len(QUOTES) - 1)
+  quote = QUOTES[quote_index]["text"]
+  author = QUOTES[quote_index]["author"]
+
+  quotesAPI = {'quote': quote, 'author': author}
+
+  #Building the context for both the APIs
+  context = {**{'cityContext': cityContext}, **weatherAPI, **quotesAPI}
+
+
+  return render(request, 'templates/index.html', context)
+
+
+
+
