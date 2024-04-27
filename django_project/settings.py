befc45f83c439b33872f9c270f6bfa72b2b99cf2
watchout254 <mukenyadaniel299@gmail.com>
2024-04-27 15:25:00 +0300

    hackathon joh

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,8 +119,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-  BASE_DIR / 'static',
+    BASE_DIR/ 'static'
 ]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- django_project/views.py
+++ django_project/views.py
@@ -1,17 +1,27 @@
 import requests
 from django.shortcuts import render
+import random
 
-def home(request):
-  # USING APIS => Example 1
-  response = requests.get('https://api.github.com/events')
-  data = response.json()
-  result = data[0]["repo"]
+def index(request):
+      # Fetch a random fact
+    fact_response = requests.get('https://uselessfacts.jsph.pl/random.json?          language=en')
+    fact_data = fact_response.json()
+    fact = fact_data['text']
 
-  # Example 2
-  reponse2 = requests.get('https://dog.ceo/api/breeds/image/random')
-  data2 = reponse2.json()
-  result2 = data2["message"]
+      # Fetch student data
+    student_response = requests.get('https://freetestapi.com/api/v1/students')
+    student_data = student_response.json()
+    students = student_data['students']
 
+      # Shuffle the list of students
+    random.shuffle(students)
 
-  
-  return render(request, 'templates/index.html', {'result': result, 'result2': result2})
+      # Get the name of the first student
+    if students:
+      first_student_name = students[0]['name']
+    else:
+      first_student_name = "No students available"
+
+      # Render the index.html template with data
+    return render(request, 'index.html', {'fact': fact, 'name':             
+      first_student_name})
